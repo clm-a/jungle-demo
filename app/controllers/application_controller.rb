8b403fe304d4facfc98b230f0ae58db0cc7b62a0
Clément Alexandre <clmntlxndr@gmail.com>
2020-01-18 13:57:40 +0100

    Base webpacker React install and prepared layout for SPA

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  def hello
-    console
-
+  def jungle_react
+    render inline: '', layout: 'jungle_react'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'application#hello'
+  root to: 'application#jungle_react'
   namespace :api do
     resources :pipelines, only: [:show, :update]
   end
